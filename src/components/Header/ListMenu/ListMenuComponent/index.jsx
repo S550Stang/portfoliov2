aca94971841107aedac7b30e39e5e583ef1ee6c8
S550Stang <prabhuvardhanrao94@gmail.com>
2022-03-19 01:27:49 +0530

    check 7

--- src/components/Header/ListMenu/ListMenuComponent/index.jsx
+++ src/components/Header/ListMenu/ListMenuComponent/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function ListMenuComponent() {
   return (
     <div className="hidden sm:inline-flex ">
-      <ul className=" flex text-white space-x-4 md:space-x-6 lg:space-x-9 xl:space-x-24 2xl:space-x-32 font-light text-xs xl:text-lg 2xl:text-xl">
+      <ul className=" flex text-white space-x-4 md:space-x-6 lg:space-x-9 xl:space-x-16 2xl:space-x-24 font-light text-xs xl:text-lg">
         <li className="hover:scale-110 transition duration-300 ease-in-out hover:underline cursor-pointer">
           <a>Home</a>
         </li>
