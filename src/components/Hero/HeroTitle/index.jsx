576b7fb84e2de81482d72421ba2c179174c7dd78
S550Stang <prabhuvardhanrao94@gmail.com>
2022-03-18 23:14:20 +0530

    fine tuning

--- src/components/Hero/HeroTitle/index.jsx
+++ src/components/Hero/HeroTitle/index.jsx
@@ -4,8 +4,8 @@ export default function HeroTitle() {
   return (
     <main>
       <div className="flex flex-col justify-center items-start p-5 space-y-10">
-        <h1 className="mt-10 text-white text-4xl inline-block">
-          Hi, i'm Prabhu. And i'm a Full Stack{" "}
+        <h1 className="mt-10 text-white text-5xl inline-block">
+          Hi, i'm Prabhu. I am a Full Stack{" "}
           <span className="text-[#3F37C9] hover:underline">Web Dev.</span>
         </h1>
         <button className="bg-[#3F37C9] text-white cursor-pointer rounded-full px-5 py-0 ">
