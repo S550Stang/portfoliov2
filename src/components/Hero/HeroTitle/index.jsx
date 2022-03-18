6fa05128ba85f5db45f3706d6b377675b9ede271
S550Stang <prabhuvardhanrao94@gmail.com>
2022-03-18 23:11:08 +0530

    check

--- src/components/Hero/HeroTitle/index.jsx
+++ src/components/Hero/HeroTitle/index.jsx
@@ -4,7 +4,7 @@ export default function HeroTitle() {
   return (
     <main>
       <div className="flex flex-col justify-center items-start p-5 space-y-10">
-        <h1 className="mt-10 text-white text-3xl inline-block">
+        <h1 className="mt-10 text-white text-4xl inline-block">
           Hi, i'm Prabhu. And i'm a Full Stack{" "}
           <span className="text-[#3F37C9] hover:underline">Web Dev.</span>
         </h1>
