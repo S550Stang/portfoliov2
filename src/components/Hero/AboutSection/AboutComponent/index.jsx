f93f3be22a2889346c4ed832bfb470b83c49d619
S550Stang <prabhuvardhanrao94@gmail.com>
2022-03-19 07:57:26 +0530

    about refactor

--- src/components/Hero/AboutSection/AboutComponent/index.jsx
+++ src/components/Hero/AboutSection/AboutComponent/index.jsx
@@ -6,19 +6,22 @@ export default function AboutComponent() {
     <div className="mt-10">
       {/** About Header */}
       <div className="relative">
-        <h1 className="text-[#3F37C9] opacity-20 font-bold text-7xl sm:text-9xl ">
+        <h1
+          className="text-[#3F37C9] opacity-20 font-bold text-7xl sm:text-9xl
+         md:text-[200px] lg:text-[250px] xl:text-[320px] 2xl:text-[400px]"
+        >
           ABOUT
         </h1>
         {/** This Will Be Flex Container */}
-        <div className="absolute left-0 right-0 top-8 lg:flex lg:justify-between lg:px-20 lg:space-x-5 2xl:px-28 w-screen ">
+        <div className="absolute left-0 right-0 top-8 sm:top-12 md:top-16 2xl:top-28 lg:flex lg:justify-between lg:px-20 lg:space-x-5 2xl:px-28 w-screen ">
           <img
-            className=" rounded-3xl mx-auto w-44 sm:w-52 md:w-60 lg:w-64 xl:w-80 2xl:w-96 lg:object-left-top lg:object-cover hover:scale-110 transition duration-300 ease-in-out"
+            className=" rounded-3xl mx-auto w-44 sm:w-52 md:w-60 lg:w-64 xl:w-80 2xl:w-2/3 lg:object-left-top lg:object-cover hover:scale-110 transition duration-300 ease-in-out"
             src={Me}
             alt="profile"
           />
           <p
-            className="inline-block p-10 font-thin text-center break-words text-white lg:text-xl
-          lg:text-left xl:text-2xl 2xl:text-4xl"
+            className="inline-block p-10 2xl:px-28 font-thin text-center break-words text-white lg:text-xl
+          lg:text-left xl:text-2xl 2xl:text-3xl 2xl:leading-10 "
           >
             Hey there! Thank you for dropping by, here's a little bit about me,
             I have a Bachelors Degree in computer science and a Post Graduate
